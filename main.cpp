9474406bce544053b7f51baf6f0ef72c3d6ced0f
Luis Rafael Sena <luisrafaelsen.20231@poli.ufrj.br>
2024-10-20 16:25:45 -0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// g++ -Dlocal main.cpp local -o main
+
+#ifdef local
+    #include "debug.h"
+    #define pr(...) debug(#__VA_ARGS__, __VA_ARGS__)
+    #define prs(...) debug_nameless(__VA_ARGS__)
+#else
+    #define pr(...) 69
+    #define prs(...) 69
+#endif
+
+#define fastio ios_base::sync_with_stdio(NULL); cin.tie(nullptr)
+
+signed main(){
+    fastio ;
+
+    return 0;
+}
